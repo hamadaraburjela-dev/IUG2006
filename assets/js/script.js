2b90d9736dfc72ef2be487908fd271d12fd8b242
hamada2210m-pixel <hamada2210m@gmail.com>
2025-09-16 13:11:54 +0300

    'DF

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 
 // غيّر للرابط الخاص بك
 // ضع رابط نشر Google Apps Script هنا 👇
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzDBr3EbMuc-nuPO4IH8wX8P8eSSxBMUxkNdBztxkcoN7N-8cJWRnf4UybsupOis9l-0w/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxvWi3rYcaaPdTxCxo3lKGTPJ5Mkb6UkU3pc2lkjun-wD1xzN9JV8YDloJdsDGgYMJvog/exec';
 
 // دالة لمزامنة النقاط مع Code.gs
 async function syncScoreToServer(uniqueId, score, currentScene = '', answeredQuestions = [], selectedGuide = '') {
